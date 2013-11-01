a71a146d0302cff17a064c3df2111e97ffdd37d6
Kevin Cao <kevincao@sumi-sumi.com>
2013-11-01 11:17:30 +0800

    fix annotation

--- SIAlertView/UIWindow+SIUtils.h
+++ SIAlertView/UIWindow+SIUtils.h
@@ -1,6 +1,6 @@
 //
 //  UIWindow+SIUtils.h
-//  SIAlertViewExample
+//  SIAlertView
 //
 //  Created by Kevin Cao on 13-11-1.
 //  Copyright (c) 2013年 Sumi Interactive. All rights reserved.
--- SIAlertView/UIWindow+SIUtils.m
+++ SIAlertView/UIWindow+SIUtils.m
@@ -1,6 +1,6 @@
 //
 //  UIWindow+SIUtils.m
-//  SIAlertViewExample
+//  SIAlertView
 //
 //  Created by Kevin Cao on 13-11-1.
 //  Copyright (c) 2013年 Sumi Interactive. All rights reserved.
