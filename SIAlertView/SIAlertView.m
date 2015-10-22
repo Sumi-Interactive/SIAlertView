85444ed0ef6861c839660b965fb502e1f44e5604
Andrea Tullis <andrea.tullis@sharewire.net>
2015-10-22 11:01:55 +0200

    iOS 9 fix

--- SIAlertView/SIAlertView.m
+++ SIAlertView/SIAlertView.m
@@ -327,6 +327,8 @@ static SIAlertView *__si_alert_current_view;
 
 + (void)showBackground
 {
+    __si_alert_background_window.rootViewController = [UIViewController new];
+    
     if (!__si_alert_background_window) {
         
         CGRect frame = [[UIScreen mainScreen] bounds];
